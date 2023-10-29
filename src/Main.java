178c369681f5c8dce2db0e57f443614a649b2da1
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-10-29 12:30:00 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int myInt = 10;
+        if(myInt>5) {
+            System.out.println("Yes!");
+        } else if (myInt<5){
+            System.out.println("NO!");
+        } else {
+            System.out.println("None ._.");
+        }
+    }
+}
